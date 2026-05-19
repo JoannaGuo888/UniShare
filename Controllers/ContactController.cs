e9712c93363a9d43b526ef823097add3337c1a93
JoannaGuo888 <jojomaimaiguo@gmail.com>
2026-05-19 11:17:38 +0100

    Admin Contact Page added

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniShare.Data;
+
+namespace UniShare.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly UniShareDbContext _context;
+        public ContactController(UniShareDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ContactController
+        public async Task<IActionResult> Index()
+        {
+            var adminUser = await _context.Users.FirstOrDefaultAsync(u => u.Role == "Admin");
+            return View(adminUser);
+        }
+
+    }
+}
