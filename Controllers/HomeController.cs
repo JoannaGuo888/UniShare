704b168b0e3f5e1b3505b001516f7bacce57c9d8
JoannaGuo888 <jojomaimaiguo@gmail.com>
2026-05-22 08:44:34 +0100

    Active&Upcoming rides for the day added to driver dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using UniShare.Data;
 using UniShare.Models;
 
 namespace UniShare.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UniShareDbContext _context;
+        public HomeController(UniShareDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,6 +22,16 @@ namespace UniShare.Controllers
             var role = HttpContext.Session.GetString("UserRole");
             ViewBag.UserRole = role;
             ViewBag.UserName = HttpContext.Session.GetString("UserName");
+            if (role == "Driver")
+            {
+                int driverId = HttpContext.Session.GetInt32("UserId") ?? 0;
+                ViewBag.TodayRides = _context.Rides
+                    .Where(r => r.DriverId == driverId
+                             && r.RideDate.Date == DateTime.Now.Date
+                             && (r.RideStatus == "Upcoming" || r.RideStatus == "Active"))
+                    .OrderBy(r => r.RideTime)
+                    .ToList();
+            }
             return View();
         }
 
