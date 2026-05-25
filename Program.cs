aae636548f94ab23da42521ba1ca26a8ec8b3fe5
JoannaGuo888 <jojomaimaiguo@gmail.com>
2026-05-25 07:27:13 +0100

    Test file removed

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using UniShare.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<UniShareDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
 
 // Add Session for login role tracking
 builder.Services.AddSession(options =>
